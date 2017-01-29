bafb5514b478a53c887ffca3bc1a72df21137111
jaschenk <jeffaschenk@gmail.com>
2017-01-29 09:54:29 -0800

    Updates to Example Run script.

--- your-microservice-example/bin/run_your_microservice_example.sh
+++ your-microservice-example/bin/run_your_microservice_example.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+#
+# Define Our SpringBoot JAR
+YMSE_JAR=your-microservice-example-1.0.0.0-SNAPSHOT-exec.jar
+DEBUG_PORT=9044
+#
+# ********************************
+# Establish Runtime Properties
+# ********************************
+PROPERTIES=""
+PROPERTIES="${PROPERTIES} -DLOG_PATH=./"
+#
+# ********************************
+# Establish Runtime JAVA OPTS
+# ********************************
+#
+JAVA_OPTS="-server -Xss4M -Xms8G -Xmx8G -Xmn2G"
+JAVA_OPTS="${JAVA_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${DEBUG_PORT}"
+#
+# Run Instance
+java ${PROPERTIES} ${JAVA_OPTS} -jar ./target/${YMSE_JAR}
